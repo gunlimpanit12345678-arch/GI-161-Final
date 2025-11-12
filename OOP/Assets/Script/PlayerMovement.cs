6e68df34a79360f5bf820462fa88e942cb1285b5
gunlimpanit12345678-arch <gunlimpanit12345678@gmail.com>
2025-11-13 01:46:58 +0700

    Movement and Camera

--- OOP/Assets/Script/PlayerMovement.cs
+++ OOP/Assets/Script/PlayerMovement.cs
@@ -4,18 +4,48 @@ public class PlayerMovement : MonoBehaviour
 {
     public float movespeed;
     Rigidbody2D rb;
-    Vector2 moveDir;
+    [HideInInspector]
+    public float lastHorizontalVector;
+    [HideInInspector]
+    public float lastVerticalVector;
+    [HideInInspector]
+    public Vector2 moveDir;
 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        InputManagement();
     }
+
+     void FixedUpdate()
+    {
+        Move();
+    }
+
+    void InputManagement() 
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector2(moveX, moveY).normalized;
+
+        if(moveDir.x !=0)
+        { lastHorizontalVector = moveDir.x; }
+        if(moveDir.y !=0)
+        { lastVerticalVector = moveDir.y; }
+    }
+
+    void Move() 
+    {
+        rb.velocity = new Vector2(moveDir.x * movespeed, moveDir.y * movespeed); 
+    }
+
+
 }
