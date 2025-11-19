82f11adb5ff45e50ea0e38613148658cb0b2e80a
gunlimpanit12345678-arch <gunlimpanit12345678@gmail.com>
2025-11-19 22:12:11 +0700

    Enemy AI

---
+++ OOP/Assets/Script/Enemies/EnemeyMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class EnemeyMovement : MonoBehaviour
+{
+    Transform player;
+    public float moveSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,player.transform.position,moveSpeed*Time.deltaTime);
+    }
+}
